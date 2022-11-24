9b19ae7fdc6a1b181223ca8dd4119a89d1cb341b
patlukas <lukaszewski.patryk06@gmail.com>
2022-11-25 00:44:35 +0100

    Fix CommentInput, change str after use ClearButton

--- src/components/FormForEditResult/FirstSection/CommentInput/index.js
+++ src/components/FormForEditResult/FirstSection/CommentInput/index.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 import { OutlinedTextField } from 'react-native-material-textfield';
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 
-const CommentInput = ({colors, rememberedComment, onChange}) => {
-    return (
-        <View style={styles.container}>
-            <OutlinedTextField
-                value={rememberedComment}
-                onEndEditing={(event) => onChange(event.nativeEvent.text)}                
-                label='Komentarz (opcjonalny)'
-                characterRestriction={100}
-                maxLength={100}
-                textColor={colors.form.input}
-                tintColor={colors.form.main}
-                baseColor={colors.form.main}
-                titleTextStyle={{bottom: 20}}
-                contentInset={styles.contentInset}
-            />
-        </View>
-    )
+class CommentInput extends Component {
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef();
+    }
+    componentDidUpdate() {
+        if(this.ref.current.value() != this.props.rememberedComment) {
+            this.ref.current.setValue(this.props.rememberedComment)
+        }
+    }
+    render() {
+        const {colors, rememberedComment, onChange} = this.props;
+        return (
+            <View style={styles.container}>
+                <OutlinedTextField
+                    ref={this.ref}
+                    value={rememberedComment}
+                    onEndEditing={(event) => onChange(event.nativeEvent.text)}                
+                    label='Komentarz (opcjonalny)'
+                    characterRestriction={100}
+                    maxLength={100}
+                    textColor={colors.form.input}
+                    tintColor={colors.form.main}
+                    baseColor={colors.form.main}
+                    titleTextStyle={{bottom: 20}}
+                    contentInset={styles.contentInset}
+                />
+            </View>
+        )
+    }
 }
-
 
 const styles = StyleSheet.create({
     container: {
